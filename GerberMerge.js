31aedb9460e6ecf2f8eb262bbab38695aa7e0b45
SaeWave <greeka+saewave@gmail.com>
2017-06-26 01:20:19 +0300

    Fix problem with drill files with excellon formt

--- GerberMerge.js
+++ GerberMerge.js
@@ -17,7 +17,10 @@ const optionDefinitions = [
     { name: 'dx', type: Number, defaultValue: 0 },
     { name: 'dy', type: Number, defaultValue: 0 },
     { name: 'units', alias: 'u', type: String, defaultValue: 'in' },
-    { name: 'postfix', alias: 'p', type: String, defaultValue: '_merged' }
+    { name: 'postfix', alias: 'p', type: String, defaultValue: '_merged' },
+    { name: 'skip-ext-check', type: Boolean, defaultValue: false },
+    { name: 'zero', type: Number, defaultValue: 'L' },
+    { name: 'places', type: String, defaultValue: '2,5' }
 ];
 
 const allowedExts = ['.gbl', '.gbo', '.gbs', '.gml', '.gtl', '.gto', '.gtp', '.txt'];
@@ -26,28 +29,35 @@ if (options.dir) {
     baseDir = options.dir;
 }
 
-//console.log(options);
-
 if (options.in == undefined) {
     console.error('Argument --in is not set!');
     process.exit(1);
 }
 
+var aPlaces = options.places.split(',');
+if (aPlaces.length == 1) {
+    options.places = [parseInt(aPlaces[0]), 5];
+} else if (aPlaces.length == 2) {
+    if (aPlaces[0] == '') {
+        options.places = [2, parseInt(aPlaces[1])];
+    } else {
+        options.places = [parseInt(aPlaces[0]), parseInt(aPlaces[1])];
+    }
+} 
+
 console.log("\nGerberMerge options:".yellow);
-console.log('    Base dir:        ', baseDir);
+console.log('    Base dir:         ', baseDir);
 console.log('    Clone:            ', options.clone);
 console.log('    dX:               ', options.dx);
 console.log('    dY:               ', options.dy);
 console.log('    Units:            ', options.units);
+console.log('    Skip ext check:   ', options['skip-ext-check']);
 console.log('    Input files/dirs:');
 for (i in options.in) {
     console.log('        "' + options.in[i] + '"');
 }
 console.log("\n");
 
-//process.exit();
-
-//console.log((baseDir + options.in[0]).green);
 for (var _in in options.in) {
     var __in = options.in[_in];
     if (fs.lstatSync(baseDir + '/' + __in).isDirectory()) {        //Process it as dir
@@ -56,14 +66,17 @@ for (var _in in options.in) {
             if (!err) {
                 
                 for (f in files) {
-                    if (!allowedExts.includes(path.extname(files[f]).toLowerCase())) continue;
+                    if (!options["skip-ext-check"] && !allowedExts.includes(path.extname(files[f]).toLowerCase())) continue;
                     console.log(('Process file: "' + options.in[0] + '/' + files[f] + '"').yellow);
-                    if (options.in.length == 1 && options.clone > 0) {
-                        const content = fs.readFileSync(baseDir + '/' + options.in[0] + '/' + files[f]);
-                        const result = merger.mergeFiles(content.toString(), options);
-                        fs.writeFileSync(baseDir + '/' + options.in[0] + '/' + files[f] + options.postfix, result);
+                    try {
+                        if (options.in.length == 1 && options.clone > 0) {
+                            const content = fs.readFileSync(baseDir + '/' + options.in[0] + '/' + files[f]);
+                            const result = merger.mergeFiles(content.toString(), options);
+                            fs.writeFileSync(baseDir + '/' + options.in[0] + '/' + files[f] + options.postfix, result);
+                        }
+                    } catch (e) {
+                        console.log('    ', e.toString().red);
                     }
-        //break;
                 }
             } else {
                 console.error(err);
